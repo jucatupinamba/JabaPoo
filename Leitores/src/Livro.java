d6d11c8090307c23b83ae922c952bf5d072c7e11
jucatupinamba <juniorbmx13@gmail.com>
2022-06-11 11:13:26 -0300

    feat: Nova aplicação- Organização e leitura de livros e usuários

---
+++ Leitores/src/Livro.java
@@ -0,0 +1,60 @@
+public class Livro {
+    //Atributos
+    private String titulo, autor;
+    private int totPaginas, pagAtual;
+    private boolean aberto;
+    private Pessoa leitor;
+    //Métodos Públicos
+    public void detalhes(){
+
+    }
+    //Métodos Especiais
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getTotPaginas() {
+        return totPaginas;
+    }
+
+    public void setTotPaginas(int totPaginas) {
+        this.totPaginas = totPaginas;
+    }
+
+    public int getPagAtual() {
+        return pagAtual;
+    }
+
+    public void setPagAtual(int pagAtual) {
+        this.pagAtual = pagAtual;
+    }
+
+    public boolean isAberto() {
+        return aberto;
+    }
+
+    public void setAberto(boolean aberto) {
+        this.aberto = aberto;
+    }
+
+    public Pessoa getLeitor() {
+        return leitor;
+    }
+
+    public void setLeitor(Pessoa leitor) {
+        this.leitor = leitor;
+    }
+}
