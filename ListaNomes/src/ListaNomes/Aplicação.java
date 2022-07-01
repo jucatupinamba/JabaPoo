189aed3f1c9af5eb3f62e36505804812f9e73606
jucatupinamba <juniorbmx13@gmail.com>
2022-07-01 00:05:35 -0300

    refactor: correção na lógica de saída de dados

--- ListaNomes/src/ListaNomes/Aplicação.java
+++ ListaNomes/src/ListaNomes/Aplicação.java
@@ -4,24 +4,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Aplicação {
-    public static void main (String [] args){
+    public static void main (String [] args) {
         List<String> lista = new ArrayList<>();
+        Processo pr = new Processo();
+
+        int b = 1;
 
         lista.add("Carlos");
         lista.add("Marcos");
         lista.add("Max");
         lista.add(1, "David");
         lista.add("Bob");
+        lista.add("Marcos");
+        lista.add("Donna");
 
         System.out.println(lista.indexOf("Marcos"));   // retorna o local do elemento pesquisado. caso false = -1
 
+        for (String x : lista) {
+
+            if (x == "Marcos"){     // problema do desafio resolvido, melhorar a lógica da impressão de dados
+                System.out.println("Nome " + x + " encontrado " + b + " vezes.");
+                b++;
+            }
 
 
-
-
-
-
-
-
+        }
     }
+
 }
+
