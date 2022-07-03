0769065377bc1c1226cd339a601d062a2ce502cf
jucatupinamba <juniorbmx13@gmail.com>
2022-07-03 09:35:18 -0300

    fix: correção saida de dados

--- ListaNomes/src/ListaNomes/Aplicação.java
+++ ListaNomes/src/ListaNomes/Aplicação.java
@@ -2,13 +2,16 @@ package ListaNomes;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Aplicação {
     public static void main (String [] args) {
         List<String> lista = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
         Processo pr = new Processo();
 
-        int b = 1;
+        int b = 0, c =0;
+        String nome;
 
         lista.add("Carlos");
         lista.add("Marcos");
@@ -18,17 +21,18 @@ public class Aplicação {
         lista.add("Marcos");
         lista.add("Donna");
 
+
         System.out.println(lista.indexOf("Marcos"));   // retorna o local do elemento pesquisado. caso false = -1
 
         for (String x : lista) {
-
-            if (x == "Marcos"){     // problema do desafio resolvido, melhorar a lógica da impressão de dados
-                System.out.println("Nome " + x + " encontrado " + b + " vezes.");
+            if (x == "Marcos"){
                 b++;
+            } else{
+                c++;
             }
-
-
         }
+        System.out.println("Nome " + " encontrado " + b + " vezes, " + c + " nomes não compativeis.");
+        System.out.println("Em uma lista de " +  lista.size() + " nomes");
     }
 
 }
