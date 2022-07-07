005771947771384546024aa49b37737a61f9ae18
jucatupinamba <juniorbmx13@gmail.com>
2022-07-07 19:58:27 -0300

    feat: novas funções da lista

--- ListaNomes/src/ListaNomes/Aplicação.java
+++ ListaNomes/src/ListaNomes/Aplicação.java
@@ -10,7 +10,8 @@ public class Aplicação {
         Scanner sc = new Scanner(System.in);
         Processo pr = new Processo();
 
-        int b = 0, c =0;
+
+        int b = 0, c = 0;
         String nome;
 
         lista.add("Carlos");
@@ -23,16 +24,18 @@ public class Aplicação {
 
 
         System.out.println(lista.indexOf("Marcos"));   // retorna o local do elemento pesquisado. caso false = -1
+        lista.set(4, "Luffy");     //Troca a string fazendo referência à posição do array
 
         for (String x : lista) {
-            if (x == "Marcos"){
+            if (x == "Marcos") {
                 b++;
-            } else{
+            } else {
                 c++;
             }
         }
         System.out.println("Nome " + " encontrado " + b + " vezes, " + c + " nomes não compativeis.");
-        System.out.println("Em uma lista de " +  lista.size() + " nomes");
+        System.out.println("Em uma lista de " + lista.size() + " nomes");
+
     }
 
 }
