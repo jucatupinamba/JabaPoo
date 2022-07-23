fcb3e9c03e4cab37235f7f38c59093a6e7e54015
jucatupinamba <juniorbmx13@gmail.com>
2022-07-23 12:26:15 -0300

    feat: add códigos completos e incompletos prática

---
+++ AgendaSalao/src/Agendamento.java
@@ -0,0 +1,35 @@
+public class Agendamento extends Servico{
+    private Integer calendario;            //Pesquisar import calendar
+    private double horario;                //Pesquisar import time
+
+    public Agendamento() {
+        super();
+        this.calendario = calendario;
+        this.horario = horario;
+    }
+
+    public Integer getCalendario() {
+        return calendario;
+    }
+
+    public void setCalendario(Integer calendario) {
+        this.calendario = calendario;
+    }
+
+    public double getHorario() {
+        return horario;
+    }
+
+    public void setHorario(double horario) {
+        this.horario = horario;
+    }
+
+    @Override
+    public String toString() {
+        return "Agendamento: " + '\n' +
+                "Nome: " + getNome() + '\n' +
+                "Telefone: " + getTelefone() + '\n' +
+                "Data: " + calendario + '\n' +
+                "Horario: " + horario;
+    }
+}
